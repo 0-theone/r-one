bdf61f0bf48cfde73c385a447721b0f87d51a080
Raissa Hohenester <raissa.developer@gmail.com>
2023-02-16 11:28:56 +0100

    Add instructions

---
+++ instructions.R
@@ -0,0 +1,10 @@
+#1- To open a script, you need to specifically tell RStudio that's what you need.
+#You do that either by clicking on the designated icon or by pressing Ctrl + Shift + N (or Cmnd + Shift + N on a Mac)
+
+# 2- In R you can manually prompt autocomplete when typing commands by pressing the Tab button on your keyboard.
+# Which elements does that work for?
+# Functions, Data Structures, Arguments of a function and Packages. Indeed, the autocomplete is a versatile,
+# all-terrain helper and you will most likely take advantage of it often
+
+#3 - Which key combination opens a new script in RStudio?
+# Ctrl + Shift + N (Mac: Cmnd + Shift + N)
